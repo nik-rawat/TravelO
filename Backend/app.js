03b639fec9da7e323c1e10ea35ce019ca8ada9ab
Keshavendra <keshavendratripathi@gmail.com>
2024-12-06 00:01:33 +0530

    again added Hello World of keshav

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send('TravelO is running!');
-=======
   res.send('Hello World of keshav');
->>>>>>> keshav
 });
 
 app.listen(port, () => {
