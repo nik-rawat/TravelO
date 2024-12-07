f4ef53835c585b2ebb4a8bb854c33ab7f6278827
nik-rawat <nr2192004@gmail.com>
2024-12-07 19:14:55 +0530

    changes done on mesh now can use html

--- Frontend/src/components/rotating-earth/rotating-earth.jsx
+++ Frontend/src/components/rotating-earth/rotating-earth.jsx
@@ -1,9 +1,10 @@
 import { useRef, useEffect } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, Stars, Text } from "@react-three/drei";
+import { OrbitControls, Stars, Html } from "@react-three/drei";
 import { a, useSpring } from "@react-spring/three";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import * as THREE from "three";
+import Hero from "./Hero";
 
 function Earth({ onLoaded }) {
   const meshRef = useRef(null);
@@ -31,18 +32,8 @@ function Earth({ onLoaded }) {
           metalness={0.3} 
           roughness={0.7} 
         />
+        <Hero />
       </mesh>
-      <Text
-        position={[0, 1.5, 0]}
-        fontSize={0.5}
-        color="white"
-        anchorX="center"
-        anchorY="middle"
-        fontWeight={700}
-        name="travelo-text" // Added name to exclude from bloom
-      >
-        Travelo
-      </Text>
     </>
   );
 }
@@ -71,12 +62,12 @@ export default function RotatingEarth({ onLoaded }) {
         <a.group scale={spring.scale}>
           <Earth onLoaded={onLoaded} />
         </a.group>
-        <Stars radius={300} depth={60} count={5000} factor={7} saturation={0} fade speed={1} />
+        <Stars radius={300} depth={60} count={5000} factor={7} saturation={0} fade speed={1.5} />
         <OrbitControls enableZoom={false} enableRotate={false} />
         <EffectComposer>
           <Bloom 
             luminanceThreshold={0.1} 
-            luminanceSmoothing={0.9} 
+            luminanceSmoothing={1.8} 
             height={300}
             kernelSize={3}
             intensity={0.5}
