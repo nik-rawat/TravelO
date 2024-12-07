5d6e9ec4c771f794461ea333d23549beafa96968
Keshavendra <keshavendratripathi@gmail.com>
2024-12-07 19:28:50 +0530

    changed the tag line

--- Frontend/src/components/rotating-earth/Hero.jsx
+++ Frontend/src/components/rotating-earth/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     >
       <div className="text-center text-white bg-transparent p-4 rounded-lg w-svw font-sans">
         <h1 className="text-4xl font-bold mb-4 font-sans">Travelo</h1>
-        <p className="text-xl mb-6 font-sans">Explore the World, One Journey at a Time</p>
+        <p className="text-xl mb-6 font-sans">From Dreaming to Exploring: We've Got You Covered</p>
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 font-sans">
           Start Your Adventure
         </button>
