c26b804addb3f2f71f1033cd21cdd7b069039e11
Keshavendra <keshavendratripathi@gmail.com>
2024-12-07 19:35:35 +0530

    changed the specifications of button

--- Frontend/src/components/rotating-earth/Hero.jsx
+++ Frontend/src/components/rotating-earth/Hero.jsx
@@ -10,9 +10,10 @@ const Hero = () => {
       <div className="text-center text-white bg-transparent p-4 rounded-lg w-svw font-sans">
         <h1 className="text-4xl font-bold mb-4 font-sans">Travelo</h1>
         <p className="text-xl mb-6 font-sans">From Dreaming to Exploring: We've Got You Covered</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 font-sans">
-          Start Your Adventure
+        <button className="bg-transparent hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 font-sans backdrop-filter backdrop-blur-md bg-opacity-30 border border-white border-opacity-50">
+         Start Your Adventure
         </button>
+
       </div>
     </Html>
   );
