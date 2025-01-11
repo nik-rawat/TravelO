87d34830e01adae7096182340984030d87500d98
nik-rawat <nr2192004@gmail.com>
2025-01-12 00:08:12 +0530

    changes vercel.json and vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,15 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+  }
 })
